70238cbd23f23b769df324729981a176ebbe9676
taichi-tsuiki <tsuiki.taichi@lmi.ne.jp>
2023-11-20 08:20:13 +0900

    コールバックの実装

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,5 @@
 class Task < ApplicationRecord
+    before_validation :set_nameless_name
     validates :name, presence: true, length: {maximum: 30}
     validate :valdate_name_not_including_comma
 
@@ -6,6 +7,10 @@ class Task < ApplicationRecord
 
     def valdate_name_not_including_comma
         errors.add(:name, 'にカンマを加えることはできません') if name&.include?(',')
+    end
+
+    def set_nameless_name
+        self.name = '名前なし' if name.blank?
     end
 
 end
